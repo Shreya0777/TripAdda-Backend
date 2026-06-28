5f1672aae656ebf7ca9eac02546b88d36ac56ee0
Shreya0777 <singhsshreya297@gmail>
2026-06-28 16:35:37 +0530

    Resolve render issue

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const rateLimit=require("express-rate-limit");
+const rateLimit = require("express-rate-limit");
 const passport = require("passport");
 const connectDb = require("./config/db");
 const dns = require("dns");
+
 const authRouter = require("./routes/authRoutes");
 const { usersRoute } = require("./routes/usersRoutes");
 const { TripRoutes } = require("./routes/TripRoutrs");
 
 const app = express();
+
+app.set("trust proxy", 1);
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 dns.setDefaultResultOrder("ipv4first");
@@ -34,6 +38,10 @@ app.use(
   })
 );
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
@@ -42,17 +50,9 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(passport.initialize());
-
 app.use("/", authRouter);
 app.use("/", usersRoute);
 app.use("/", TripRoutes);
-
-
 
 connectDb()
   .then(() => {
